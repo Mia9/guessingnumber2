47f009769181f4f04202a89f8669aa7cc48a2e62
Lim <limhsiuying90@gmail.com>
2023-08-16 13:50:43 +0800

    third commit

--- guessnumber2.rb
+++ guessnumber2.rb
@@ -1,34 +1,53 @@
+scoring_card = Hash.new    #use hash to create new list, list from user input
+                           #new hash put outside of loop so can keep the score
+loop do
+  puts "Hello whats your name?"
+  player_name = gets.chomp
+  puts "Hello #{player_name}"
 
-secret_number = rand(100)
-#puts secret_number
-puts "Insert any number from 1 to 100"
+  secret_number = rand(100)
+  puts secret_number
+  puts "Insert any number from 1 to 100"
 
-#while loop method, x means number of tries
+  # while loop method, x means number of tries
 
-x = 1 
+  tries = 1 
+  guessed = []
 
-while x <= 6
-	answer = gets.chomp.to_i #to_i means change to numbers or integers
-	puts answer 
+  while tries <= 5
+  	answer = gets.chomp.to_i #to_i means change to numbers or integers
+  	puts answer 
 
-	if answer < secret_number
-	    puts "Too Low"
-    
+    if answer < secret_number
+  	  puts "Too Low"
     elsif answer > secret_number
       puts "Too High"
-
     else answer == secret_number
       puts "WIN"  
       break
-
     end
-#second commit test git
-    puts "x: #{x}"
-    x += 1
 
+    guessed << answer
+
+    puts "guessed: #{guessed}"
+    puts "tries: #{tries}"
+    tries += 1 
+  end
+
+  scoring_card[player_name] = tries
+      
+  scoring_card.each do |tries| 
+    puts scoring_card.sort_by(&:last) #sort by values low to high 
+    puts "#{tries}"
+  end    
+
+  puts "Do you want to play again? y/n " 
+  play = gets.chomp.downcase
+
+  if play != "y"  #this part means if answer is not y, the loop continue on new game
+    break 
+  end
 end
-
-
 
 
 
